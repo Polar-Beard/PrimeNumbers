db1a80bbd8092c1cd6c3aab00945ea76797dbe40
Polar-Beard <saramontecino@outlook.com>
2016-06-24 10:54:39 -0700

    Add src file.

---
+++ PrimeNumbers.java
@@ -0,0 +1,49 @@
+public class PrimeNumbers
+{
+  private static final int NUM = 1000;
+  private static final int FIRST_PRIME = 2;
+  private static int[] primes;
+  private static int primesCount;
+
+  private static void fillPrimesArray()
+  {
+    for(int i = 3; i <= NUM; i++) //Starts at 3 since 2 has already been done.
+    {
+      if(isPrime(i))
+      {
+        primes[primesCount] = i;
+        primesCount++;
+      }
+    }
+  }
+
+  private static boolean isPrime(int number)
+  {
+    for(int i = 0; i < primesCount; i++)
+    {
+      if(number % primes[i] == 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static void printArray()
+  {
+    for(int i = 0; i < primesCount; i++)
+    {
+      System.out.println(primes[i]);
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    primes = new int[NUM];
+    primes[0] = FIRST_PRIME; //Add the first prime to the array
+    primesCount = 1; //Starts at 1 since the first prime is in the array
+
+    fillPrimesArray();
+    printArray();
+  }
+}
